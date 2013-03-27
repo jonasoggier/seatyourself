e3cd5c3d5035459226860dd1de052c1e4be07749
Jon Aziz <jonaziz@gmail.com>
2013-03-27 09:25:05 -0400

    Initial app setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seatyourself::Application.config.secret_token = 'd70b6d9f09497ee663bbe3b709a0d097911d851e05fee9b0646365d47f140e9356363d6959a6ac73c195edf1b8227ea0b2d8ba16396d5d4fc476bcb5f6e5bdda'
